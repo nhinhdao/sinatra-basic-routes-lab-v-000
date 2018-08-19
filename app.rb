e1759d74d9563ab12f1d37e2667e3af773d8f15a
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-08-19 18:49:21 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,23 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-
+   
    get "/" do
       "Hello, World!"
    end
-
+   
    get "/name" do |n|
-   status 200
+      status 200
       puts "My name is #{n}"
    end
-
+   
    get "/hometown" do |n|
-   status 200
+      status 200
       "My hometown is #{n}"
    end
-
+   
    get "/favorite-song" do |n|
-   status 200
+      status 200
       "My favorite song is #{n}"
    end
 end
