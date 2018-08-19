d8fda2f87cacaad109af66502e5ec6ce61006213
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-08-19 19:09:31 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+   get "/" do
+      "Hello, World!"
+   end
+
+   get "/name" do
+      # halt 200
+      "My name is Robin"
+   end
+
+   get "/hometown" do
+      # halt 200
+      "My hometown is Thai Binh, Vietnam"
+   end
+
+   get "/favorite-song" do
+      # halt 200
+      "My favorite song is SUteki Da Ne -  in Final Fantasy X"
+   end
 end
